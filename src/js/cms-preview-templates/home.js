a25c15f21024623184a6ef3819a3a884d9587d71
diealteschwedin <104922277+diealteschwedin@users.noreply.github.com>
2022-05-09 22:49:28 +0200

    Update home.js

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -36,7 +36,7 @@ export default class PostPreview extends React.Component {
             </div>
 
             <div className="tc">
-              <a href="#" className="btn raise">See all products</a>
+              <a href="#" className="btn raise">Alle Angebote ansehen</a>
             </div>
           </div>
         </div>
@@ -57,7 +57,7 @@ export default class PostPreview extends React.Component {
             </div>
 
             <div className="tc">
-              <a href="{{.buttonLink}}" className="btn raise">Read more</a>
+              <a href="{{.buttonLink}}" className="btn raise">Mehr erfahren</a>
             </div>
 
           </div>
